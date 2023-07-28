aee3a09247c56be700fc8e7aa395caa40e6db6cb
alex <mgpyaephyolay@gmail.com>
2023-07-28 10:37:38 -0500

    added navigation with intents

--- app/src/main/java/com/example/jul21mvvmrickandmorty/Constants.kt
+++ app/src/main/java/com/example/jul21mvvmrickandmorty/Constants.kt
@@ -3,4 +3,6 @@ package com.example.jul21mvvmrickandmorty
 object Constants {
     const val PAGE_SIZE = 20
     const val PREFETCH_DISTANCE = 2 * PAGE_SIZE
+
+    const val INTENT_EXTRA_CHARACTER_ID = "character_id"
 }
