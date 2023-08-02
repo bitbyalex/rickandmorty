5b0c2c15ed5bde8b4faf71b8feaaa47bc685715e
alex <mgpyaephyolay@gmail.com>
2023-08-02 17:38:10 -0500

    migrating Activities to Fragments

--- app/src/main/java/com/example/jul21mvvmrickandmorty/CharacterDetailsFragment.kt
+++ app/src/main/java/com/example/jul21mvvmrickandmorty/CharacterDetailsFragment.kt
@@ -5,14 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
+import com.airbnb.epoxy.EpoxyRecyclerView
 
 class CharacterDetailsFragment : Fragment() {
+
+    private val viewModel :  ShareViewModel by lazy {
+        ViewModelProvider(this)[ShareViewModel::class.java]
+    }
+
+    private val epoxyController = CharacterDetailsEpoxyController()
+
+    private val safeArgs : CharacterDetailsFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_character_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.characterByIdResponseLiveData.observe(viewLifecycleOwner) { character ->
+            epoxyController.character = character
+            if (character == null) {
+                Toast.makeText(requireActivity(), "Unsuccessful network call", Toast.LENGTH_SHORT)
+                    .show()
+                return@observe
+            }
+        }
+
+        viewModel.refreshByCharacter(safeArgs.characterId)
+        val epoxyRecyclerView = view.findViewById<EpoxyRecyclerView>(R.id.epoxyRecyclerView)
+        epoxyRecyclerView.setControllerAndBuildModels(epoxyController)
     }
 
 }
--- app/src/main/java/com/example/jul21mvvmrickandmorty/CharacterListFragment.kt
+++ app/src/main/java/com/example/jul21mvvmrickandmorty/CharacterListFragment.kt
@@ -1,12 +1,22 @@
 package com.example.jul21mvvmrickandmorty
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.airbnb.epoxy.EpoxyRecyclerView
+import com.example.jul21mvvmrickandmorty.characters.CharacterListEpoxyController
+import com.example.jul21mvvmrickandmorty.characters.CharactersViewModel
 
 class CharacterListFragment : Fragment() {
+
+    private val epoxyController = CharacterListEpoxyController(::onCharacterSelected)
+    private val viewModel : CharactersViewModel by lazy {
+        ViewModelProvider(this)[CharactersViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,12 +25,18 @@ class CharacterListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_character_list, container, false)
     }
 
-    override fun onResume() {
-        super.onResume()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        view?.postDelayed({
-            val navGraphActivity = activity as NavGraphActivity
-            navGraphActivity.navController.navigate(R.id.action_characterListFragment_to_characterDetailsFragment)
-        },3000)
+        viewModel.characterPagedListLiveData.observe(viewLifecycleOwner){pagedList ->
+            epoxyController.submitList(pagedList)
+        }
+
+        view.findViewById<EpoxyRecyclerView>(R.id.epoxyRecyclerView).setController(epoxyController)
+    }
+
+    private fun onCharacterSelected(characterId : Int){
+        val direction = CharacterListFragmentDirections.actionCharacterListFragmentToCharacterDetailsFragment(characterId)
+        findNavController().navigate(direction)
     }
 }
